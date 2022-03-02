e54eb7e36fab8c0dbe411d564f80c03e1271dc0a
Daniel Adedeji <adedejiayokunledaniel@gmail.com>
2022-03-02 21:51:28 +0100

    code refactor

--- assigment.c
+++ assigment.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
 //we have simple and compound assigment
 
 //simple x = x + 20;
 //compound x+= 20;
 
 //
+
+int main(){
+    return 0;
+}
--- incrementanddecrement.c
+++ incrementanddecrement.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+
 // ++num1 means x = x + 1; pre increment means the increment will be done immediately on the variable
 // --num1 means x = x - 1; pre decrement means the decrement will be done immediately on the variable
 
 
 // num1++ means x = x + 1; post increment means the increment will be done on the next occurence of the variable
 // num1-- means x = x - 1; pre decrement means the decrement will be done on the next occurence of the variable
+
+
+int main(){
+
+    return 0;
+}
