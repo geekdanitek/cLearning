deceaea1d49ab053ac0f12da765ba35223697131
Daniel Adedeji <adedejiayokunledaniel@gmail.com>
2022-03-02 22:09:12 +0100

    folder refactor

---
+++ example/areaAndCircumference.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+
+    double radius;
+    const double Pi = 3.142;
+
+    printf("Enter your radius: ");
+    scanf("%lf", &radius);
+
+    double areaOfCircle = Pi * radius * radius;
+
+    double circumference = 2 * Pi * radius;
+
+    printf("The area of this circle is: %lf \n", areaOfCircle);
+    printf("The circumference of this circle is: %lf \n", circumference);
+
+
+
+    return 0;
+}
